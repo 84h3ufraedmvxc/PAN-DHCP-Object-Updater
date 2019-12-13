fad977439b756612b8e42f3b8ad5e6efece1214d
84h3ufraedmvxc <58835745+84h3ufraedmvxc@users.noreply.github.com>
2019-12-13 13:25:19 +1000

    first commit

---
+++ config_1.1.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+##########################
+#
+#
+# PAN DHCP Interface Address Object Updater
+# Configuration Script 
+# Version 1.1
+#
+##########################
+
+# Change Log
+# 1.1
+# - Updated to include steps to capture firewall details from user for variables in 'panobjectupdate' script
+script="panobjectupdate_1.2.sh"
+
+echo ""
+printf "      Welcome to the Palo Alto Networks DHCP Interface Address Object Updater configuration tool\n"
+echo ""
+echo ""
+printf "This script requires the following tools installed on order to function...\n"
+printf "     'xmllint' - Utilisted for parsing of XML configuration data\n"
+printf "     'gnupg' - Utilised for secure storage of API key\n"
+echo ""
+echo ""
+read -p "Press any key to continue..."
+echo ""
+echo ""
+echo "-----------------------------------------------------------------------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo ""
+printf "To securely store your API key, a GNUPG key pair must be generated. Before proceeding, please generate a new key pair \n"
+printf "with the with the below paramaters... \n"
+echo ""
+echo ""
+printf "Key-Type: RSA\n"
+printf "Key-Length: 4096\n"
+printf "Subkey-Type: RSA\n"
+printf "Subkey-Length: 4096\n"
+printf "Name-Real: panops\n"
+printf "Expire-Date: 0\n"
+echo ""
+echo ""
+printf "NOTE: This tool is best utilised with crontab. Thus this key pair MUST NOT UTILISE A PASSPHRASE in order to run non-interactivly.\n"
+echo ""
+echo ""
+printf "If you have not created the key-pair, please close this script (cntrl-c) and do so before proceeding. Otherwise please proceed...\n"
+echo ""
+echo ""
+read -p "Press any key to continue..."
+
+
+###############		Might perform key gen as part of script later
+# printf "A new GPG key pair will now be generated for the purpose of surely storing your API key\n"
+# echo ""
+# echo ""
+# read -p "Press any key to continue..."
+# export gpghome="$(mktemp -d)"
+# cat > gpgpan <<EOF
+# Key-Type: RSA
+# Key-Length: 4096
+# Subkey-Type: RSA
+# Subkey-Length: 4096
+# Name-Real: panops
+# Expire-Date: 0
+# %commit
+# EOF
+# gpg --batch --gen-key gpgpan
+# sleep 5s
+# printf "A new GPG key pair has been generated. Use the below command to view this key (panops)...\new"
+# printf "     gpg --list-keys"
+
+printf "What is your firewalls hostname/IP?\n"
+read -p 'Hostname/IP: ' hostname
+echo ""
+echo ""
+printf "Which administrative account owns the API key generated during configuration?\n"
+read -p 'Administrator: ' administrator
+echo ""
+echo ""
+printf "Name the DHCP client interface want monitored. (E.g ethernet1/1 or ae1.200)\n"
+read -p 'DHCP Interface: ' dhcpinterface
+echo ""
+echo ""
+printf "Name the Address object which is currently defined to be that of "$dhcpinterface"\n"
+read -p 'Address Object: ' addressobject
+echo ""
+echo ""
+sed -i -e "s/hostname=.*/hostname="\"$hostname\""/" $script
+sed -i -e "s/administrator=.*/administrator="\"$administrator\""/" $script
+sed -i -e "s/dhcpinterface=.*/dhcpinterface="\"$dhcpinterface\""/" $script
+sed -i -e "s/addressobject=.*/addressobject="\"$addressobject\""/" $script
+echo ""
+echo ""
+echo "-----------------------------------------------------------------------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------------------------------------------------------"
+echo ""
+echo ""
+printf "Please enter the API key for an administrative account of your firewall with limited priviledges to perform the following via the API...\n"
+printf "     Operational Commands\n"
+printf "     Configuration\n"
+printf "     Commit\n"
+printf "This account SHOULD NOT have any GUI or CLI access!\n"
+read -sp 'API Key: ' apikey
+echo ""
+printf "Please enter the name of the GPG key you wish to encrypt your API key with...\n"
+read -p 'GPG Key Name: ' gpgkey
+echo "--------------------------------------------------Encrypting with GPG-------------------------------------------------------"
+echo $apikey | gpg --encrypt --armor -r $gpgkey > apikey.gpg
+echo "----------------------------------------------------------------------------------------------------------------------------"
+echo ""
+printf "Assuming no errors with GPG (see above), Your API key has been stored securely... kinda\n"
+
+
---
+++ panobjectupdate_1.2.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+##########################
+#
+#
+# PAN DHCP Interface Address Object Updater 
+# Version 1.2
+#
+#
+##########################
+
+# Changelog
+# 1.1
+# - Added cleanup tasks
+# - Altered to read DHCP IP directly from firewall as opposed to using ipinfo.io/ip
+# 1.2
+# - Semi-secure storage of API key integrated
+
+# Firewall Variables
+hostname="panfw.skim.systems.local"
+administrator="apiadmin"
+dhcpinterface="ethernet1/1"
+addressobject="ip_nbn_eth1"
+
+echo ""
+echo ""
+
+# Decrypt API Key
+apikey=$(gpg --decrypt apikey.gpg)
+echo ""
+echo ""
+
+# Testing
+echo "API Key is "$apikey""
+
+# IPv4 Variables
+dhcpip=""
+currentobject=""
+
+# Obtain Raw XML from PAN which contains the configuration of the $addressobject object and store it in 'addrobject.xml'
+curl -kgs "https://$hostname/api/?type=config&action=get&xpath=/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/address/entry[@name='$addressobject']&key=$apikey" > addrobject.xml
+# Obtain raw XML from PAN which contains the current IP address allocated by DHCP. Stored in 'intdetails.xml'
+curl -kgs "https://$hostname/api/?type=op&cmd=<show><interface>$dhcpinterface</interface></show>&key=$apikey" > intdetails.xml
+
+# Bind IP from Object from ip-netmask Xpath to variable minus the /32 CIDR mask
+ipcidr=$(xmllint --xpath '//ip-netmask/text()' addrobject.xml)
+currentobject=${ipcidr%/*}
+# Bind IP from DHCP interface from 'dyn-addr' Xpath to variable minus any CIDR mask
+dhcpipcidr=$(xmllint --xpath '//ifnet/dyn-addr/member/text()' intdetails.xml)
+dhcpip=${dhcpipcidr%/*}
+
+# Testing
+echo "Live IP is "$dhcpip""
+echo "Current object is set to "$currentobject""
+
+# If the IP's do not match, update the address object with the correct IP address and perform a partial commit of the changes
+if [ $currentobject != $dhcpip ]; then
+	printf "\n$(date) | IP CHANGE DETECTED!\nPushing updated address object configuration\n" >> log.txt
+	curl -kgs "https://$hostname/api/?type=config&action=set&key=$apikey&xpath=/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/address/entry[@name='$addressobject']&element=<ip-netmask>$dhcpip/32</ip-netmask>" >> log.txt
+	sleep 5s
+	printf "\n$(date) | Commiting changes\n" >> log.txt
+	curl -kgs "https://$hostname/api/?type=commit&cmd=<commit><partial><admin><member>$administrator</member></admin></partial></commit>&key=$apikey" >> log.txt
+	printf "\n" >> log.txt
+else	
+	printf "$(date) | PAN Address Object reflects current IP allocated by DHCP\n" >> log.txt
+fi
+
+printf "Finished. Please check log.txt for any API errors\n"
+
+# Cleanup
+rm -f addrobject.xml
+rm -f intdetails.xml
